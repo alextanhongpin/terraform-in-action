3029f43cfd9e21233dfd6ee02d6fc98a729a3386
alextanhongpin <alextan220990@gmail.com>
2021-06-03 00:46:28 +0800

    chore: generate multiple files

--- 03-functional-programming/madlib.tf
+++ 03-functional-programming/madlib.tf
@@ -22,37 +22,59 @@ variable "words" {
   }
 }
 
+variable "num_files" {
+  default = 100
+  type    = number
+}
+
 locals {
   uppercase_words = { for k, v in var.words : k => [for s in v : upper(s)] }
 }
 
 resource "random_shuffle" "random_nouns" {
+  count = var.num_files
   input = local.uppercase_words["nouns"]
 }
 
 resource "random_shuffle" "random_adjectives" {
+  count = var.num_files
   input = local.uppercase_words["adjectives"]
 }
 
 resource "random_shuffle" "random_verbs" {
+  count = var.num_files
   input = local.uppercase_words["verbs"]
 }
 
 resource "random_shuffle" "random_adverbs" {
+  count = var.num_files
   input = local.uppercase_words["adverbs"]
 }
 
 resource "random_shuffle" "random_numbers" {
+  count = var.num_files
   input = local.uppercase_words["numbers"]
 }
 
-output "mad_libs" {
-  value = templatefile("${path.module}/templates/alice.txt",
-    {
-      nouns      = random_shuffle.random_nouns.result
-      adjectives = random_shuffle.random_adjectives.result
-      verbs      = random_shuffle.random_verbs.result
-      adverbs    = random_shuffle.random_adverbs.result
-      numbers    = random_shuffle.random_numbers.result
+locals {
+  templates = tolist(fileset(path.module, "templates/*.txt"))
+}
+
+resource "local_file" "mad_libs" {
+  count    = var.num_files
+  filename = "madlibs/madlibs-${count.index}.txt"
+  content = templatefile(element(local.templates, count.index), {
+    nouns      = random_shuffle.random_nouns[count.index].result,
+    adjectives = random_shuffle.random_adjectives[count.index].result,
+    verbs      = random_shuffle.random_verbs[count.index].result,
+    adverbs    = random_shuffle.random_adverbs[count.index].result,
+    numbers    = random_shuffle.random_numbers[count.index].result,
   })
+}
+
+data "archive_file" "mad_libs" {
+  depends_on  = [local_file.mad_libs]
+  type        = "zip"
+  source_dir  = "${path.module}/madlibs"
+  output_path = "${path.cwd}/madlibs.zip"
 }
